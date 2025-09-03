a3065fd032e90c3c4fe4775f93dd1b4084f25c2d
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2025-09-03 18:56:50 +0200

    💄 implement slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Slider } from "@/components/ui/slider";
 
 export default function Home() {
   return (
     <div>
-      <h1 className={"font-medium text-2xl"}>Personal info</h1>
+      <Slider defaultValue={[20]} min={8} max={100} step={1} />
+      <h1 className={"mt-8 font-medium text-2xl"}>Personal info</h1>
       <form className={"mt-8 grid gap-y-6"}>
         <div>
           <p>First Name</p>
