a315c50a4cfe0bdc019843f457b4416fa8e0481b
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2025-09-03 17:02:42 +0200

    💄 adjust button design

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import type * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-[5px] text-lg font-medium cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-[5px] text-lg font-medium cursor-pointer transition-all disabled:pointer-events-none disabled:bg-purple-300 focus-visible:ring-purple-300 focus-visible:ring-[3px] outline-none",
   {
     variants: {
       variant: {
